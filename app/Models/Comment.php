8b1413700c977b26a61f98b704ed603fe863bafa
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2025-12-02 12:16:04 +0700

    comment: add comment model and migration

---
+++ app/Models/Comment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    protected $table = 'comments';
+
+    protected $fillable = [
+        'user_id',
+        'post_id',
+        'content',
+    ];
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
